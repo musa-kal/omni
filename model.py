5e86593ab3737f26a7d77dec4fceee8b3cffb10b
Musa Kaleem <131704022+Musa-Kal@users.noreply.github.com>
2025-11-09 17:22:00 -0500

    code refactor

--- model.py
+++ model.py
@@ -42,18 +42,38 @@ class Layers:
                 z = self.activation_function(z)
             return z
 
-    def __init__(self, first_layer: BaseLayer):
-        self.layers = [first_layer]
+    def __init__(self, input_shape:tuple):
+        self.input_shape = tuple(input_shape)
+        self.layers = []
         self.weights = []
+
+    def __init_first_layer(self, layer:BaseLayer):
+        if len(layer.shape) != len(self.input_shape):
+            raise ValueError(f"new layer shape {layer.shape} doesn't match the input shape {self.input_shape}!")
+        self.layers.append(layer)
+        self.weights.append(np.empty(shape=(self.input_shape[0], layer.shape[0]), dtype=NP_FLOAT_PRECISION))
+
+
+
     
     def join_front(self, new_layer: BaseLayer):
+        if len(self.layers) == 0:
+            self.__init_first_layer(new_layer)
+            return
         if len(new_layer.shape) != len(self.layers[-1].shape):
             raise ValueError(f"new layer shape {new_layer.shape} doesn't match the previous layer {self.layers[-1].shape}!")
         self.layers.append(new_layer)
         self.weights.append(np.empty(shape=(new_layer.shape[0], self.layers[-1].shape[0]), dtype=NP_FLOAT_PRECISION))
 
+    def feedforward(self, input_data):
+        layer_outputs = []
+
+        curr_input = input_data
+        for i, layer in enumerate(self.layers):
+            pass 
+
     def __repr__(self):
-        output = "layers: ["
+        output = f"layers: [Input-{self.input_shape}\t" 
         for layer in self.layers:
             output += f"{layer}\t"
         return output.strip() + "]"
@@ -65,7 +85,7 @@ class Layers:
 if __name__ == '__main__':
     input = Layers.DenseLayer(1)
     print(input.feedforward([1]))
-    x = Layers(input)
+    x = Layers(input_shape=[1])
     x.join_front(Layers.DenseLayer(3))
     print(x)
     pass
